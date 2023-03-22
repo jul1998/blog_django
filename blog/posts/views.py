9f39c510da1ba96024e265e83155a7060cd2c874
jul1998 <jguevara321@gmail.com>
2023-03-21 23:29:38 -0600

    Initial commit

---
+++ blog/posts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def index(request):
+    posts =  Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
+
+def post(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'post.html', {'post': post})
